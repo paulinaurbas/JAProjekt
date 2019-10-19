f8785dd1239f3cdb261f784a86a75d21444315b8
paulinaurbas <urbas.paulina@gmail.com>
2019-10-19 19:51:33 +0200

    Second project meeting

---
+++ JA_project/ProjectApp/DLLCPlusPlus/DLLCPlusPlus.h
@@ -0,0 +1,5 @@
+#pragma once
+extern "C"
+{
+	__declspec(dllexport) void Negative(unsigned char* bmp, int begin, int end, int width);
+}
---
+++ JA_project/ProjectApp/ProjectApp/ProjectApp.cpp
@@ -0,0 +1,42 @@
+// ProjectApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <windows.h>
+#include "tchar.h"
+#include "ReadBMP.h"
+#include <windows.h>
+
+#include <iostream>
+extern "C" int _stdcall MyProc1(DWORD x, DWORD y);
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <array>
+#include <vector>
+#include <iterator>
+
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+/*	int x = 3, y = 4, z = 0;
+	z = MyProc1(x, y);  // wywo³anie procedury asemblerowej z biblioteki
+	std::cout << z << std::endl;
+	*/
+	
+	
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file

c4a710d320d45b594c5e0cf156b6eab7b284371e
paulinaurbas <urbas.paulina@gmail.com>
2019-10-20 16:42:05 +0200

    new things

---
+++ JA_project/JaProjectWindowsApp/JaProjectWindowsApp/MainWidnow.h
@@ -0,0 +1,65 @@
+#pragma once
+
+namespace JaProjectWindowsApp {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MainWidnow
+	/// </summary>
+	public ref class MainWidnow : public System::Windows::Forms::Form
+	{
+	public:
+		MainWidnow(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MainWidnow()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// MainWidnow
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(831, 514);
+			this->Name = L"MainWidnow";
+			this->Text = L"MainWidnow";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
---
+++ JA_project/JaProjectWindowsApp/JaProjectWindowsApp/stdafx.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "MainWidnow.h"
+
+// TODO: reference additional headers your program requires here
